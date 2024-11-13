00e0c4b0e3c413d2401882178b3a230a23f19ce3
stepan <stepan.zakolyukin@yandex.ru>
2024-11-13 15:51:06 +0500

    Добавил тест на корректное создание экземпляра класса. Добавил код в конструктор класса

--- cs/TagsCloudVisualization/CircularCloudLayouter.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Drawing;
-
-namespace TagsCloudVisualization;
-
-internal interface CircularCloudLayouter
-{
-    CircularCloudLayouter(Point center);
-    Rectangle PutNextRectangle(Size rectangleSize);
-}
---
+++ cs/TagsCloudVisualization/Task 1/CircularCloudLayouterTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Drawing;
+
+
+namespace TagsCloudVisualization;
+
+
+[TestFixture]
+public class CircularCloudLayouterTests
+{
+    [Test]
+    public void CircularCloudLayouter_CorrectInitialization_NoExceptions()
+    {
+        var createAConstructor = () => new CircularCloudLayouter(new Point(960, 540));
+
+        createAConstructor
+            .Should()
+            .NotThrow();
+    }
+}
