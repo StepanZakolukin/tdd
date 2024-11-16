2aa2d0824ac047ac39181c391df1db78470a9102
stepan <stepan.zakolyukin@yandex.ru>
2024-11-16 19:13:09 +0500

    Сделал визуализацию

---
+++ cs/TagsCloudVisualization/Task 2/CloudLayout.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization.Task_2;
+
+public static class CloudLayout
+{ 
+    public static IEnumerable<Rectangle> GenerateCloudLayout(int amountRectangles,
+        int startWidthRectangles, int endWidthRectangles,
+        int startHeightRectangles, int endHeightRectangles,
+        CircularCloudLayouter cloudLayouter)
+        => GenerateRectangleSizes(amountRectangles,
+            startWidthRectangles, endWidthRectangles,
+            startHeightRectangles, endHeightRectangles)
+            .Select(cloudLayouter.PutNextRectangle);
+
+    private static IEnumerable<Size> GenerateRectangleSizes(
+        int amountData,
+        int startWidth, int endWidth,
+        int startHeight, int endHeight)
+    {
+        var random = new Random();
+
+        for (var i = 0; i < amountData; i++)
+        {
+            var width = random.Next(startWidth, endWidth + 1);
+            var height = random.Next(startHeight, endHeight + 1);
+
+            yield return new Size(width, height);
+        }
+    }
+}
---
+++ cs/TagsCloudVisualization/Task 2/VisualizationCloudLayout.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+
+namespace TagsCloudVisualization.Task_2;
+
+public class VisualizationCloudLayout
+{
+    public readonly int Width;
+    public readonly int Height;
+    public readonly Color BackgroundColor;
+    public readonly Color[] RectanglePalette;
+
+    public VisualizationCloudLayout(int width, int height, Color backgroundColor, IEnumerable<Color> rectanglePalette)
+    {
+        Width = width;
+        Height = height;
+        BackgroundColor = backgroundColor;
+        RectanglePalette = rectanglePalette.ToArray();
+    }
+
+    public Bitmap CreateAnImage(Point center, int amountRectangles,
+        int startWidthRectangles, int endWidthRectangles,
+        int startHeightRectangles, int endHeightRectangles)
+    {
+        var image = new Bitmap(Width, Height);
+        var rectangles = CloudLayout.GenerateCloudLayout(
+            amountRectangles,
+            startWidthRectangles, endWidthRectangles,
+            startHeightRectangles, endHeightRectangles,
+            new CircularCloudLayouter(center));
+        var graphics = Graphics.FromImage(image);
+        
+        DrawCloudLayout(graphics, rectangles);
+
+        return image;
+    }
+
+    private void DrawCloudLayout(Graphics graphics, IEnumerable<Rectangle> rectangles)
+    {
+        var random = new Random();
+
+        graphics.FillRectangle(new SolidBrush(BackgroundColor), 0, 0, Width, Height);
+
+        foreach (var rect in rectangles)
+        {
+            var color = RectanglePalette[random.Next(RectanglePalette.Length)];
+
+            graphics.FillRectangle(new SolidBrush(color), rect);
+            graphics.DrawRectangle(new Pen(Color.Black, 1), rect);
+        }
+    }
+}
